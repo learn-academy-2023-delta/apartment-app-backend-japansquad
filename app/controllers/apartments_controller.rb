d3093948033c1470808aaac0be9eb0667d489898
immikeoa <immikeoa@gmail.com>
2023-07-06 13:41:01 -0400

    adds create method and its test.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,8 +1,27 @@
 class ApartmentsController < ApplicationController
 
+
     def index
         apartments = Apartment.all
         render json: apartments
     end
-    
-end
+
+    def create
+      
+      apartment = Apartment.create(apartment_params)
+
+  
+      if apartment.valid?
+        render json: apartment, status: :created
+      else
+        render json: apartment.errors, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def apartment_params
+      params.require(:apartment).permit(:street, :unit, :prefecture, :city, :square_footage, :price, :bedrooms, :bathrooms, :pets, :amenities, :image, :user_id)
+    end
+  end
+  
